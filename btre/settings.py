e53d342872b622cfaa10b4fe1cab602c0526f9f2
brokenclocks <bernardleigh94@gmail.com>
2020-10-01 01:28:15 -0500

    vid 49 setup

--- btre/settings.py
+++ btre/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
